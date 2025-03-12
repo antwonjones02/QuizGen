4f2449e9e55c0d19e022b838446fc65d7dbab3c3
Antwon Jones <antwonjones02@users.noreply.github.com>
2025-03-12 17:05:28 -0400

    Add Redux store configuration

---
+++ client/src/store/index.ts
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from '../services/api';
+import authReducer from './slices/authSlice';
+import documentsReducer from './slices/documentsSlice';
+import quizReducer from './slices/quizSlice';
+import uiReducer from './slices/uiSlice';
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    auth: authReducer,
+    documents: documentsReducer,
+    quiz: quizReducer,
+    ui: uiReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
