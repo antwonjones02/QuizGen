3c4f955e49c35b1acf334730345dfb93664914ff
Antwon Jones <antwonjones02@users.noreply.github.com>
2025-03-12 17:21:00 -0400

    Update models index to include Feedback model

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,13 +2,15 @@ import { User } from './user.model';
 import { Document } from './document.model';
 import { Quiz } from './quiz.model';
 import { QuizAttempt } from './quizAttempt.model';
+import { Feedback } from './feedback.model';
 
 // Re-export all models
 export {
   User,
   Document,
   Quiz,
-  QuizAttempt
+  QuizAttempt,
+  Feedback
 };
 
 // Export interfaces for type checking
@@ -17,3 +19,4 @@ export type { IDocument } from './document.model';
 export type { IQuiz } from './quiz.model';
 export type { IQuestion } from './question.model';
 export type { IQuizAttempt } from './quizAttempt.model';
+export type { IFeedback } from './feedback.model';
